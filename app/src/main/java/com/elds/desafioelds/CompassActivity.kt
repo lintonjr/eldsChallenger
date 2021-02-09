ba33e54fb3e136b7a2811fa299b34bf6e4666702
Linton <linton@motorola.com>
2021-02-09 15:01:06 -0400

    refactor: change navigation intents

---
+++ app/src/main/java/com/elds/desafioelds/CompassActivity.kt
@@ -0,0 +1,11 @@
+package com.elds.desafioelds
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CompassActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_compass)
+    }
+}
--- app/src/main/java/com/elds/desafioelds/MainActivity.kt
+++ app/src/main/java/com/elds/desafioelds/MainActivity.kt
@@ -1,21 +1,41 @@
 package com.elds.desafioelds
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
-    var compass : CardView? = null
-    var biometric : CardView? = null
-    var proximity : CardView? = null
-    var step : CardView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        compass = findViewById(R.id.compass)
-        biometric = findViewById(R.id.biometric)
-        proximity = findViewById(R.id.proximity)
-        step = findViewById(R.id.step)
+        val compass = findViewById<CardView>(R.id.compass)
+        val biometric = findViewById<CardView>(R.id.biometric)
+        val proximity = findViewById<CardView>(R.id.proximity)
+        val step = findViewById<CardView>(R.id.step)
+
+        compass.setOnClickListener {
+            val intent = Intent(this, CompassActivity::class.java)
+            startActivity(intent)
+        }
+
+        biometric.setOnClickListener {
+            val intent = Intent(this, BiometricActivity::class.java)
+            startActivity(intent)
+        }
+
+        proximity.setOnClickListener {
+            val intent = Intent(this, BiometricActivity::class.java)
+            startActivity(intent)
+        }
+
+        step.setOnClickListener {
+            val intent = Intent(this, BiometricActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
