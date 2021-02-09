2789b5921cba15fea8021d43fda78ed6e8533368
Linton <linton@motorola.com>
2021-02-09 16:46:17 -0400

    refactor: change layout step counter

--- app/src/main/java/com/elds/desafioelds/StepActivity.kt
+++ app/src/main/java/com/elds/desafioelds/StepActivity.kt
@@ -1,68 +1,97 @@
 package com.elds.desafioelds
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowManager
 import android.widget.TextView
+import android.widget.Toast
+import com.mikhaellopez.circularprogressbar.CircularProgressBar
 
 class StepActivity : AppCompatActivity(), SensorEventListener {
 
-    private lateinit var stepCounter: TextView
-    private lateinit var stepDetector: TextView
-    private lateinit var sensorManager: SensorManager
-    private lateinit var mStepCounter: Sensor
-    private var isCounterSensorPresent: Boolean = false
-    var stepCount: Int = 0
+    private var sensorManager: SensorManager? = null
 
-    @SuppressLint("ServiceCast")
+    private var running = false
+    private var totalSteps = 0f
+    private var previousTotalSteps = 0f
+
+    private lateinit var tv_steps: TextView
+    private lateinit var progress_circular: CircularProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_step)
 
-        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        loadData()
+        resetSteps()
 
-        this.stepCounter = findViewById(R.id.stepCounter)
-        this.stepDetector = findViewById(R.id.stepDetector)
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
 
-        this.sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+    }
 
-        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null) {
-            mStepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
-            isCounterSensorPresent = true
-            stepCounter.text = stepCount.toString()
+    override fun onResume() {
+        super.onResume()
+        running = true
+        val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        if (stepSensor == null) {
+            Toast.makeText(this, "No Sensor detected on this device", Toast.LENGTH_SHORT).show()
         } else {
-            stepCounter.text = "Counter Sensor is not present"
-            isCounterSensorPresent = false
+            sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI)
         }
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
-        if(event?.sensor == mStepCounter) {
-            stepCount = event.values[0].toInt()
-            stepCounter.text = stepCount.toString()
+        if (running) {
+            totalSteps = event!!.values[0]
+            val currentSteps = totalSteps.toInt() - previousTotalSteps.toInt()
+            tv_steps = findViewById(R.id.tv_steps)
+            tv_steps.text = "$currentSteps"
+
+            progress_circular = findViewById(R.id.progress_circular)
+            progress_circular.apply {
+                setProgressWithAnimation(currentSteps.toFloat())
+            }
         }
+    }
+
+    private fun resetSteps(){
+        tv_steps = findViewById(R.id.tv_steps)
+        tv_steps.setOnClickListener {
+            Toast.makeText(this, "Long tap to reset steps", Toast.LENGTH_SHORT).show()
+        }
+
+        tv_steps.setOnLongClickListener {
+            previousTotalSteps = totalSteps
+            tv_steps.text = 0.toString()
+            saveData()
+            true
+        }
+    }
+
+    private fun saveData() {
+        val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putFloat("key1", previousTotalSteps)
+        editor.apply()
+    }
+
+    private fun loadData() {
+        val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        val savedNumber = sharedPreferences.getFloat("key1", 0f)
+        Log.d("StepActivity", "$savedNumber")
+        previousTotalSteps = savedNumber
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
         TODO("Not yet implemented")
     }
 
-    override fun onResume() {
-        super.onResume()
-        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null) {
-            sensorManager.registerListener(this, mStepCounter, SensorManager.SENSOR_DELAY_NORMAL)
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null) {
-            sensorManager.unregisterListener(this, mStepCounter)
-        }
-    }
 }
