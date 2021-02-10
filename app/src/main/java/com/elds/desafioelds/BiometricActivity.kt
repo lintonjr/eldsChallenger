09e2d287e31a971b06428071abfd9b1800594feb
Micael Pimentel <micael.pimentel1@gmail.com>
2021-02-10 11:01:00 -0300

    Implementado metodos biometria

--- app/src/main/java/com/elds/desafioelds/BiometricActivity.kt
+++ app/src/main/java/com/elds/desafioelds/BiometricActivity.kt
@@ -1,11 +1,163 @@
 package com.elds.desafioelds
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.app.KeyguardManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.fingerprint.FingerprintManager
 import android.os.Bundle
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyPermanentlyInvalidatedException
+import android.security.keystore.KeyProperties
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import java.io.IOException
+import java.security.*
+import java.security.cert.CertificateException
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.NoSuchPaddingException
+import javax.crypto.SecretKey
+
 
 class BiometricActivity : AppCompatActivity() {
+
+    lateinit var imageView: ImageView
+
+    private val KEY_NAME = "CHAVE_EXEMPLO"
+    private lateinit var fingerprintManager: FingerprintManager
+    private lateinit var keyguardManager: KeyguardManager
+    private lateinit var keyStore: KeyStore
+    private lateinit var keyGenerator: KeyGenerator
+    private lateinit var cipher: Cipher
+    private lateinit var cryptoObject: FingerprintManager.CryptoObject
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_biometric)
+
+        imageView = findViewById<ImageView>(R.id.imagem_biometric)
+
+        keyguardManager = getSystemService(KEYGUARD_SERVICE) as KeyguardManager
+        fingerprintManager = getSystemService(Context.FINGERPRINT_SERVICE) as FingerprintManager
+
+        if (!keyguardManager.isKeyguardSecure()) {
+            Toast.makeText(this,
+                "Lock screen security not enabled in Settings",
+                Toast.LENGTH_LONG).show()
+        }
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.USE_FINGERPRINT) !=
+            PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this,
+                "Fingerprint authentication permission not enabled",
+                Toast.LENGTH_LONG).show()
+
+            return
+        }
+
+        if (!fingerprintManager.hasEnrolledFingerprints()) {
+
+            Toast.makeText(this,
+                "Register at least one fingerprint in Settings",
+                Toast.LENGTH_LONG).show()
+            return
+        }
+
+        generateKey()
+
+        if (cipherInit()) {
+            cryptoObject = FingerprintManager.CryptoObject(cipher)
+            val helper = FingerprintHandler(this)
+            helper.startAuth(fingerprintManager, cryptoObject)
+        }
+
+    }
+
+    protected fun generateKey() {
+        try {
+            keyStore = KeyStore.getInstance("AndroidKeyStore")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        keyGenerator = try {
+            KeyGenerator.getInstance(
+                KeyProperties.KEY_ALGORITHM_AES,
+                "AndroidKeyStore"
+            )
+        } catch (e: NoSuchAlgorithmException) {
+            throw RuntimeException(
+                "Failed to get KeyGenerator instance", e
+            )
+        } catch (e: NoSuchProviderException) {
+            throw RuntimeException(
+                "Failed to get KeyGenerator instance", e
+            )
+        }
+        try {
+            keyStore.load(null)
+            keyGenerator.init(
+                KeyGenParameterSpec.Builder(
+                    KEY_NAME,
+                    KeyProperties.PURPOSE_ENCRYPT or
+                            KeyProperties.PURPOSE_DECRYPT
+                )
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                    .setUserAuthenticationRequired(true)
+                    .setEncryptionPaddings(
+                        KeyProperties.ENCRYPTION_PADDING_PKCS7
+                    )
+                    .build()
+            )
+            keyGenerator.generateKey()
+        } catch (e: NoSuchAlgorithmException) {
+            throw RuntimeException(e)
+        } catch (e: InvalidAlgorithmParameterException) {
+            throw RuntimeException(e)
+        } catch (e: CertificateException) {
+            throw RuntimeException(e)
+        } catch (e: IOException) {
+            throw RuntimeException(e)
+        }
+    }
+
+    fun cipherInit(): Boolean {
+        cipher = try {
+            Cipher.getInstance(
+                KeyProperties.KEY_ALGORITHM_AES + "/"
+                        + KeyProperties.BLOCK_MODE_CBC + "/"
+                        + KeyProperties.ENCRYPTION_PADDING_PKCS7
+            )
+        } catch (e: NoSuchAlgorithmException) {
+            throw java.lang.RuntimeException("Failed to get Cipher", e)
+        } catch (e: NoSuchPaddingException) {
+            throw java.lang.RuntimeException("Failed to get Cipher", e)
+        }
+        return try {
+            keyStore.load(null)
+            val key: SecretKey = keyStore.getKey(
+                KEY_NAME,
+                null
+            ) as SecretKey
+            cipher.init(Cipher.ENCRYPT_MODE, key)
+            true
+        } catch (e: KeyPermanentlyInvalidatedException) {
+            false
+        } catch (e: KeyStoreException) {
+            throw java.lang.RuntimeException("Failed to init Cipher", e)
+        } catch (e: CertificateException) {
+            throw java.lang.RuntimeException("Failed to init Cipher", e)
+        } catch (e: UnrecoverableKeyException) {
+            throw java.lang.RuntimeException("Failed to init Cipher", e)
+        } catch (e: IOException) {
+            throw java.lang.RuntimeException("Failed to init Cipher", e)
+        } catch (e: NoSuchAlgorithmException) {
+            throw java.lang.RuntimeException("Failed to init Cipher", e)
+        } catch (e: InvalidKeyException) {
+            throw java.lang.RuntimeException("Failed to init Cipher", e)
+        }
     }
 }
--- app/src/main/java/com/elds/desafioelds/FingerprintHandler.kt
+++ app/src/main/java/com/elds/desafioelds/FingerprintHandler.kt
@@ -1,4 +1,88 @@
 package com.elds.desafioelds
 
-class FingerprintHandler {
+import android.Manifest
+import android.R
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.fingerprint.FingerprintManager
+import android.media.Image
+import android.os.CancellationSignal
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+
+
+class FingerprintHandler(val context: Context): FingerprintManager.AuthenticationCallback() {
+
+    private lateinit var cancellationSignal: CancellationSignal
+    private var appContext: Context
+
+    init {
+        appContext = context
+    }
+
+    fun startAuth(
+        manager: FingerprintManager,
+        cryptoObject: FingerprintManager.CryptoObject?
+    ) {
+        cancellationSignal = CancellationSignal()
+        if (ActivityCompat.checkSelfPermission(
+                appContext,
+                Manifest.permission.USE_FINGERPRINT
+            ) !=
+            PackageManager.PERMISSION_GRANTEDhelper.put
+        ) {
+            return
+        }
+        manager.authenticate(cryptoObject, cancellationSignal, 0, this, null)
+    }
+
+    override fun onAuthenticationError(
+        errMsgId: Int,
+        errString: CharSequence
+    ) {
+        super.onAuthenticationError(errMsgId,errString)
+        Toast.makeText(
+            appContext,
+            "Authentication error\n$errString",
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+    override fun onAuthenticationHelp(
+        helpMsgId: Int,
+        helpString: CharSequence
+    ) {
+        Toast.makeText(
+            appContext,
+            "Authentication help\n$helpString",
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+    override fun onAuthenticationFailed() {
+        super.onAuthenticationFailed()
+        Toast.makeText(
+            appContext,
+            "Authentication failed",
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+    override fun onAuthenticationSucceeded(
+        result: FingerprintManager.AuthenticationResult?
+    ) {
+        super.onAuthenticationSucceeded(result)
+        Toast.makeText(
+            appContext,
+            "Authentication succeeded",
+            Toast.LENGTH_LONG
+        ).show()
+
+        Log.d("Biometrico:",result.toString())
+    }
 }
